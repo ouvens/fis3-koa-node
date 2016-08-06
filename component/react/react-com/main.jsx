047ae0992760b7a837b5e2786862688b86ea3023
ouvens <1025224452@qq.com>
2016-08-06 09:16:05 +0800

    add

--- component/react/react-com/main.jsx
+++ component/react/react-com/main.jsx
@@ -5,5 +5,17 @@ var HelloMessage = React.createClass({
     	return <div>Hello {this.props.name}</div>;
   	}
 });
- 
+
+render.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="about" component={App}/>
+      <Route path="users" component={App}>
+        <Route path="/user/:userId" component={App}/>
+      </Route>
+      <Route path="*" component={App}/>
+    </Route>
+  </Router>
+), document.getElementById('root'))
+
 module.exports = HelloMessage;
