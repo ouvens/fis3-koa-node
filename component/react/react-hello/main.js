850927aa702d3e18849112c28a3cab34701224a7
ouvens <1025224452@qq.com>
2016-12-01 17:37:55 +0800

    add

--- component/react/react-hello/main.js
+++ component/react/react-hello/main.js
@@ -4,14 +4,33 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ReactContent = React.createClass({
-
     /**
      * props用于组件接受外部参数, 需要设置给state
      * @return {[type]} [description]
      */
     getInitialState: function() {
+        console.log(this.props);
         return this.props;
     },
+
+    /**
+     * 默认的生命周期函数，生命周期图示如下
+     * http://upload-images.jianshu.io/upload_images/326507-281c610cec06a015.png
+     * @return {[type]} [description]
+     */
+    componentDidMount: function() {
+        $.get(this.props.getUrl, function(result) {
+            var lastGist = result[0];
+            console.log(result);
+            // if (this.isMounted()) {
+            // this.setState({
+            //   username: lastGist.owner.login,
+            //   lastGistUrl: lastGist.html_url
+            // });
+            // }
+        }.bind(this));
+    },
+
 
     change: function(){
         this.setState({data: this.props.data1});
@@ -62,7 +81,7 @@ module.exports = {
 
         ReactDOM.render(
             // <ReactContent url="/api/comments" /> // 从服务端获取数据
-            <ReactContent data={data} data1={data1}/>,
+            <ReactContent data={data} data1={data1} getUrl="http://127.0.0.1:8086/org_rank.html"/>,
             document.getElementById('testHello')
         );
 	}
