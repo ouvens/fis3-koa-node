e6888672932d97ea905b678b9f5498788a78524e
ouvens <1025224452@qq.com>
2016-12-23 10:44:39 +0800

    add

--- component/react/react-hello/main.jsx
+++ component/react/react-hello/main.jsx
@@ -2,39 +2,7 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-/*
-var Redux = require('redux');
 
-const store = Redux.createStore(reducer);
-
-function reducer(state, action) {
-	state = state || {};
-    if(action.type){
-
-    	state[action.type] = action.data;
-    }
-    return state
-};
-
-store.dispatch({
-    type: 'content',
-    data: {
-        name: 'content-name-init',
-        address: 'content-address-init',
-        age: '26',
-        job: 'content-job-init'
-    }
-});
-
-store.dispatch({
-    type: 'hello',
-    data: { 
-        name: 'hello-name-init',
-        address: 'hello-address-init',
-        age: '26',
-        job: 'hello-job-init'
-    }
-});*/
 
 var HelloMessage = React.createClass({
     /**
@@ -46,34 +14,6 @@ var HelloMessage = React.createClass({
         return { data: data };
     },
 
-    /**
-     * 默认的生命周期函数，生命周期图示如下
-     * http://upload-images.jianshu.io/upload_images/326507-281c610cec06a015.png
-     * @return {[type]} [description]
-     */
-    // componentDidMount: function() {
-    //     var self = this;
-    //     $.get(this.props.getUrl, function(result) {
-    //         var lastGist = result[0];
-    //         // console.log(result);
-    //         // if (this.isMounted()) {
-    //         // this.setState({
-    //         //   username: lastGist.owner.login,
-    //         //   lastGistUrl: lastGist.html_url
-    //         // });
-    //         // }
-    //     }.bind(this));
-
-    //     var store = this.props.store;
-
-    //     function handleChange() {
-    //         self.setState({ data: store.getState()['hello'] });
-    //     }
-
-    //     let unsubscribe = store.subscribe(handleChange);
-    //     // unsubscribe();
-    // },
-    // 
     change: function () {
     	// body...
     },
